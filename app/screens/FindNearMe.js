99e26b9766f7c041ce1f4eb2a97ce168c85d9e1c
Ryan Yogan <ryanyogan@gmail.com>
2018-01-26 15:13:34 -0800

    wired up the basics for geo location

--- app/screens/FindNearMe.js
+++ app/screens/FindNearMe.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { func } from 'prop-types';
+import { connectAlert } from '../components/Alert';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import LocateMeButton from '../components/LocateMeButton';
 
 class FindNearMe extends Component {
+  static propTypes = {
+    alertWithType: func,
+  };
+
+  handleGeolocationSuccess = position => {
+    console.log(`Lat: ${position.coords.latitude}`);
+    console.log(`Long: ${position.coords.longitude}`);
+  };
+
+  handleGeolocationError = error =>
+    this.props.alertWithType('error', 'Error', error.message);
+
+  goToNearMe = () => {
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeolocationSuccess,
+      this.handleGeolocationError,
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  };
+
   render() {
     return (
       <Container>
-        <Header>Find Near Me</Header>
+        <LocateMeButton onPress={this.goToNearMe} />
+        <Header>Find Nearest Charging Stations</Header>
       </Container>
     );
   }
 }
 
-export default FindNearMe;
+export default connectAlert(FindNearMe);
