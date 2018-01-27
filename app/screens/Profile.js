38e65ac497f9dee4d467f5fb43f1a9f1a1b633d9
Ryan Yogan <ryanyogan@gmail.com>
2018-01-26 19:24:31 -0800

    implemented signOut

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { NavigationActions } from 'react-navigation';
+import Meteor from 'react-native-meteor';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import { PrimaryButton } from '../components/Form';
 
 class Profile extends Component {
+  signOut = () => {
+    Meteor.logout(() => {
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({ routeName: 'SignUp' })],
+      });
+      this.props.navigation.dispatch(resetAction);
+    });
+  };
+
   render() {
     return (
       <Container>
         <Header>Profile</Header>
+
+        <PrimaryButton title="Sign Out" onPress={this.signOut} />
       </Container>
     );
   }
