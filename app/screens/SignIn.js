82e7cdd0420b097fcbc7c4d3d6b9573e53e7c6cf
Ryan Yogan <ryanyogan@gmail.com>
2018-01-26 19:32:55 -0800

    added in signIn logic

--- app/screens/SignIn.js
+++ app/screens/SignIn.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import Meteor from 'react-native-meteor';
+import { NavigationActions } from 'react-navigation';
 import { Card } from 'react-native-elements';
 import { Input, PrimaryButton } from '../components/Form';
 import Container from '../components/Container';
-import { Header } from '../components/Text';
+import { connectAlert } from '../components/Alert';
 
 class SignIn extends Component {
   state = {
     emailOrUsername: '',
     password: '',
+    loading: false,
+  };
+
+  signIn = () => {
+    const { emailOrUsername, password } = this.state;
+
+    if (emailOrUsername.length === 0) {
+      return this.props.alertWithType(
+        'error',
+        'Error',
+        'Email or Username is required.',
+      );
+    }
+
+    if (password.length === 0) {
+      return this.props.alertWithType(
+        'error',
+        'Error',
+        'Password is required.',
+      );
+    }
+
+    this.setState({ loading: true });
+
+    return Meteor.loginWithPassword(emailOrUsername, password, err => {
+      this.setState({ loading: false });
+      if (err) {
+        return this.props.alertWithType('error', 'Error', err.reason);
+      }
+
+      const resetAction = NavigationActions.reset({
+        index: 0,
+        actions: [NavigationActions.navigate({ routeName: 'Profile' })],
+      });
+      this.props.navigation.dispatch(resetAction);
+    });
   };
 
   render() {
@@ -28,11 +66,15 @@ class SignIn extends Component {
             onChangeText={password => this.setState({ password })}
             value={this.state.password}
           />
-          <PrimaryButton title="Sign In" />
+          <PrimaryButton
+            title="Sign In"
+            loading={this.state.loading}
+            onPress={this.signIn}
+          />
         </Card>
       </Container>
     );
   }
 }
 
-export default SignIn;
+export default connectAlert(SignIn);
