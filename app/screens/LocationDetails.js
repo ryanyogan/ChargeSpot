54cb53c432fced5f16c207052dd8081b72f95664
Ryan Yogan <ryanyogan@gmail.com>
2018-01-26 17:04:12 -0800

    HOC wrapper for Meteor on LD, subscribe to updates from locations pub

--- app/screens/LocationDetails.js
+++ app/screens/LocationDetails.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Button, Card } from 'react-native-elements';
 import { Text } from 'react-native';
+import Meteor, { createContainer } from 'react-native-meteor';
 import _ from 'lodash';
 import colors from '../config/colors';
 import Container from '../components/Container';
 
 class LocationDetails extends Component {
   render() {
-    const { location } = this.props.navigation.state.params;
+    const location =
+      this.props.location ||
+      _.get(this.props, 'navigation.state.params.location', {});
+
+    console.log(location._id);
+
     const userId = _.get(this.props, 'user._id', '');
     const checkedIn = location.checkedInUserId === userId;
     const available = typeof location.checkedInUserId !== 'string';
@@ -30,18 +36,29 @@ class LocationDetails extends Component {
         <Card title={location.station_name}>
           <Text>{location.street_address}</Text>
           <Text>{location.access_days_time}</Text>
+          <Button
+            raised
+            icon={icon}
+            title={title}
+            backgroundColor={backgroundColor}
+            buttonStyle={{ marginVertical: 20 }}
+            disabled={!available && !checkedIn}
+          />
         </Card>
-        <Button
-          raised
-          icon={icon}
-          title={title}
-          backgroundColor={backgroundColor}
-          buttonStyle={{ marginVertical: 20 }}
-          disabled={!available && !checkedIn}
-        />
       </Container>
     );
   }
 }
 
-export default LocationDetails;
+const ConnectedLocationDetails = createContainer(params => {
+  const location = _.get(params, 'navigation.state.params.location', {});
+
+  Meteor.subscribe('Locations.pub.details', { locationId: location._id });
+
+  return {
+    user: Meteor.user(),
+    location: Meteor.collection('locations').findOne({ _id: location._id }),
+  };
+}, LocationDetails);
+
+export default ConnectedLocationDetails;
